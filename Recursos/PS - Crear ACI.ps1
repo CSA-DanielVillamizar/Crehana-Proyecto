e8fc79ee137183dc517c03626b34ca710f22e462
Daniel Villamizar <daniel8106@hotmail.com>
2020-10-21 16:33:55 -0500

    Update PS - Crear ACI.ps1

--- Recursos/PS - Crear ACI.ps1
+++ Recursos/PS - Crear ACI.ps1
@@ -1,33 +1,22 @@
 ﻿<# Definición
 Cuando los usuarios piensan en la virtualización, a menudo les viene a la mente las máquinas virtuales 
 (VM). En realidad, la virtualización puede tomar muchas formas y los contenedores son una de ellas. 
+
 ¿Cuál es la diferencia entre las máquinas virtuales y los contenedores?
 
 A grandes rasgos, las máquinas virtuales virtualizan el hardware subyacente para que se puedan ejecutar
-varias instancias de sistemas operativos (SO) en el hardware. Cada máquina virtual ejecuta un sistema 
-operativo y tiene acceso a recursos virtualizados que representan el hardware subyacente.
+varias instancias de sistemas operativos (SO) en el hardware. C
 
-Las máquinas virtuales tienen muchas ventajas. Entre ellas se incluyen la capacidad de ejecutar 
-diferentes sistemas operativos en el mismo servidor, el uso más eficaz y rentable de los recursos 
-físicos y el aprovisionamiento de servidores más rápido. Por otra parte, cada máquina virtual contiene 
-una imagen de sistema operativo, bibliotecas, aplicaciones, etc., por lo que puede ser bastante grande.
-
-Un contenedor virtualiza el sistema operativo subyacente y hace que la aplicación en contenedor perciba 
+Un contenedor virtualiza el sistema operativo y hace que la aplicación en contenedor perciba 
 que tiene el sistema operativo (incluidas la CPU, la memoria, el almacenamiento de archivos y las 
-conexiones de red) todo para ella sola. Dado que se abstraen las diferencias en el sistema operativo y
-la infraestructura subyacentes, siempre que la imagen base sea coherente, el contenedor se puede 
-implementar y ejecutar en cualquier lugar. Para los desarrolladores, esto es sumamente interesante.
+conexiones de red) todo para ella sola. 
 
-Puesto que los contenedores comparten el sistema operativo host, no necesitan arrancar un sistema 
-operativo ni cargar bibliotecas. Esto permite que los contenedores sean mucho más eficientes y ligeros.
-Las aplicaciones en contenedor se pueden iniciar en segundos y pueden caber muchas más instancias de la 
-aplicación en la máquina en comparación con un escenario de máquina virtual. El enfoque de un sistema 
-operativo compartido tiene la ventaja adicional de reducir la sobrecarga en términos de mantenimiento, 
-como la aplicación de revisiones y actualizaciones.
+Esto permite que los contenedores sean mucho más eficientes y ligeros.
 
 ACI
 
-Azure Container Instances resulta útil para escenarios que pueden funcionar en contenedores aislados, lo que incluye aplicaciones simples, automatización de tareas y trabajos de compilación. Estas son algunas de las ventajas:
+Azure Container Instances resulta útil para escenarios que pueden funcionar en contenedores aislados, 
+lo que incluye aplicaciones simples, automatización de tareas y trabajos de compilación. 
 
 Inicio rápido: inicie los contenedores en cuestión de segundos.
 Facturación por segundo: solo se generan gastos mientras se ejecuta el contenedor.
@@ -37,6 +26,7 @@ Tamaños personalizados: especifique los valores exactos de núcleos de CPU y me
 Almacenamiento persistente: monte recursos compartidos de Azure Files directamente en un contenedor 
 para recuperar y conservar el estado.
 Linux y Windows: programe contenedores Windows y Linux con la misma API.
+
 En aquellos escenarios donde se necesita una orquestación completa de contenedores, lo que incluye 
 la detección de servicios en varios contenedores, el escalado automático y las actualizaciones de 
 aplicaciones coordinadas, se recomienda Azure Kubernetes Service (AKS).
@@ -51,7 +41,7 @@ mantiene los nodos del agente.
 
 #>
 
-New-AzResourceGroup -Name <resource-group-name> -Location <resource-group-location> #use this command when you need to create a new resource group for your deployment
-New-AzResourceGroupDeployment -ResourceGroupName <resource-group-name> -TemplateUri https://raw.githubusercontent.com/CSA-DanielVillamizar/Crehana-Proyecto/master/Recursos/aci-linuxcontainer-public-ip/azuredeploy.json
+New-AzResourceGroup -Name ACI-RG-01 -Location "eastus" 
+New-AzResourceGroupDeployment -ResourceGroupName ACI-RG -TemplateUri https://raw.githubusercontent.com/CSA-DanielVillamizar/Crehana-Proyecto/master/Recursos/aci-linuxcontainer-public-ip/azuredeploy.json
 
 
