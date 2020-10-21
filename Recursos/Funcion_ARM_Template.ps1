22c6ab399f93d0a8515b3447af5c06698097ea32
Daniel Villamizar <daniel8106@hotmail.com>
2020-10-21 02:43:57 -0500

    Create Funcion_ARM_Template.ps1

---
+++ Recursos/Funcion_ARM_Template.ps1
@@ -0,0 +1,8 @@
+﻿$resourceGroupName = Read-Host -Prompt "Escriba un nombre de grupo de recursos que se utilice para generar nombres de recursos"
+$location = Read-Host -Prompt "Introduzca la ubicación (Como 'eastus' or 'northeurope')"
+$templateUri = "https://raw.githubusercontent.com/CSA-DanielVillamizar/Crehana-Proyecto/master/Recursos/function-app-create-dynamic/azuredeploy.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri
+
+Read-Host -Prompt "Pulse [ENTER] para continuar ..."
