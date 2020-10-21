a9d7fee8b2193828000ce2e6075396074f20175c
Daniel Villamizar <daniel8106@hotmail.com>
2020-10-21 10:15:39 -0500

    Inicial

---
+++ Recursos/PS- Crear Conjunto escalado.ps1
@@ -0,0 +1,97 @@
+﻿<# Definición
+El conjunto de escalado de máquinas virtuales le permite implementar y administrar 
+un conjunto de máquinas virtuales de escalabilidad automática. Puede escalar el número 
+de máquinas virtuales del conjunto de escalado manualmente o definir reglas de escalado 
+automático según el uso de recursos tales como la CPU, la demanda de memoria o el tráfico 
+de red. Un equilibrador de carga de Azure distribuirá el tráfico a las instancias de 
+máquina virtual del conjunto de escalado. 
+#>
+
+# Crear RG
+New-AzResourceGroup -ResourceGroupName "Creahana-rg-Scaleset" -Location "EastUS"
+
+# Crear conjunto de escalado de máquinas virtuales 
+
+New-AzVmss `
+  -ResourceGroupName "Creahana-rg-Scaleset" `
+  -Location "EastUS" `
+  -VMScaleSetName "ScaleSet-Prod" `
+  -VirtualNetworkName "VM-Vnet-Prod" `
+  -SubnetName "VM-Subnet-FrontEnd" `
+  -PublicIpAddressName "VM-PublicIPAddress" `
+  -LoadBalancerName "VM-SS-LoadBalancer" `
+  -UpgradePolicyMode "Automatic"
+
+
+# Crear aplicación web prueba
+
+# Definir el script para que se ejecute la extensión de script personalizado
+$publicSettings = @{
+    "fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate-iis.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File automate-iis.ps1"
+}
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+            -ResourceGroupName "Creahana-rg-Scaleset" `
+            -VMScaleSetName "ScaleSet-Prod"
+
+# Use Custom Script Extension para instalar IIS y configurar el sitio web básico
+Add-AzVmssExtension -VirtualMachineScaleSet $vmss `
+    -Name "customScript" `
+    -Publisher "Microsoft.Compute" `
+    -Type "CustomScriptExtension" `
+    -TypeHandlerVersion 1.8 `
+    -Setting $publicSettings
+
+# Actualice el conjunto de escalado y aplique la extensión de script personalizado a las instancias de máquina virtual
+Update-AzVmss `
+    -ResourceGroupName "Creahana-rg-Scaleset" `
+    -Name "ScaleSet-Prod" `
+    -VirtualMachineScaleSet $vmss
+
+# Permitir tráfico a la aplicación
+
+# Obtener información sobre el conjunto de escalado
+$vmss = Get-AzVmss `
+            -ResourceGroupName "Creahana-rg-Scaleset" `
+            -VMScaleSetName "ScaleSet-Prod"
+
+#Crear una regla para permitir el tráfico a través del puerto 80
+$nsgFrontendRule = New-AzNetworkSecurityRuleConfig `
+  -Name FrontendNSGRule `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 200 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+
+#Cree un grupo de seguridad de red y asócielo a la regla
+$nsgFrontend = New-AzNetworkSecurityGroup `
+  -ResourceGroupName  "Creahana-rg-Scaleset" `
+  -Location EastUS `
+  -Name FrontendNSG `
+  -SecurityRules $nsgFrontendRule
+
+$vnet = Get-AzVirtualNetwork `
+  -ResourceGroupName "Creahana-rg-Scaleset" `
+  -Name VM-Vnet-Prod
+
+$frontendSubnet = $vnet.Subnets[0]
+
+$frontendSubnetConfig = Set-AzVirtualNetworkSubnetConfig `
+  -VirtualNetwork $vnet `
+  -Name VM-Subnet-FrontEnd `
+  -AddressPrefix $frontendSubnet.AddressPrefix `
+  -NetworkSecurityGroup $nsgFrontend
+
+Set-AzVirtualNetwork -VirtualNetwork $vnet
+
+# Actualice el conjunto de escalado y aplique la extensión de script personalizado a las instancias de máquina virtual
+Update-AzVmss `
+    -ResourceGroupName "Creahana-rg-Scaleset" `
+    -Name "ScaleSet-Prod" `
+    -VirtualMachineScaleSet $vmss
---
+++ Recursos/PS-Crear VM.ps1
@@ -0,0 +1,46 @@
+﻿# Iniciar Sesión
+Connect-AzAccount
+
+# Variables para valores comunes
+$resourceGroup = "Crehana-RG-VM"
+$location = "eastus"
+$vmName = "SRV-WEB-01"
+
+# Crear objeto de usuario
+$cred = Get-Credential -Message "Introduzca un nombre de usuario y una contraseña para la máquina virtual."
+
+# Crear un grupo de recursos
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Crear una configuración de subred
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name Subnet-FE-Prod -AddressPrefix 172.0.1.0/24
+
+# Crear una red virtual
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroup -Location $location `
+  -Name vNET-Prod -AddressPrefix 172.0.0.0/16 -Subnet $subnetConfig
+
+# Cree una dirección IP pública y especificar un nombre DNS
+$pip = New-AzPublicIpAddress -ResourceGroupName $resourceGroup -Location $location `
+  -Name "vmpublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+
+# Cree una regla de grupo de seguridad de red de entrada para el puerto 3389
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name vmNetworkSecurityGroupRuleRDP  -Protocol Tcp `
+  -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+  -DestinationPortRange 3389 -Access Allow
+
+# Crear un grupo de seguridad de red
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
+  -Name vmNetworkSecurityGroup -SecurityRules $nsgRuleRDP
+
+# Cree una tarjeta de red virtual y asóciese con la dirección IP pública y NSG
+$nic = New-AzNetworkInterface -Name VMNic -ResourceGroupName $resourceGroup -Location $location `
+  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Crear una configuración de máquina virtual
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize Standard_A2_v2 | `
+Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest | `
+Add-AzVMNetworkInterface -Id $nic.Id
+
+# Crear una máquina virtual
+New-AzVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
